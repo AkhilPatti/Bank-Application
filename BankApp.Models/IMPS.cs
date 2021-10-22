0e4adf48339ecc6dd9b02d19ea20763d33213014
AkhilPatti <91309633+AkhilPatti@users.noreply.github.com>
2021-10-22 18:44:40 +0530

    Update IMPS.cs

--- BankApp.Models/IMPS.cs
+++ BankApp.Models/IMPS.cs
@@ -9,7 +9,7 @@ namespace BankApp.Models
     class IMPS : Transaction
     {
         private int sameAccountCharge = 5; //charges for transcation between same BankAccounts
-        private int chargesDifferent = 6; //charges for transcation between same BankAccounts
+        private int otherAccountCharge = 6; //charges for transcation between same BankAccounts
         public int minLimit = 2; 
         public int maxLimit = 400000;
 
