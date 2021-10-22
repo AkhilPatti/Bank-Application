5e045b20b1088fbdad8a201c944c49bdd9ff62fa
AkhilPatti <radhaakhilpatti@gmail.com>
2021-10-22 10:29:32 +0530

    Added Models Reagarding new Task Update

---
+++ BankApp.Models/RTGS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Models
+{
+    class RTGS : Transaction
+    {
+        public int minLimit = 400000;
+        private float chargesSame = 0; //charges for transcation between same BankAccounts
+        private float chargesDifferent = 2; //charges for transcation between different BankAccounts
+    }
+}
---
+++ BankAppCLI2/Capture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BankApp.Models;
+namespace BankApp.CLI2
+{
+    public static class Capture
+    {
+        public static  Genders CaptureGender()
+        {
+            Console.WriteLine("Enter your your Gender\n1. Male\n2.Female\n3.TransGender");
+            Genders gender = (Genders)Enum.Parse(typeof(Genders), Console.ReadLine());
+            return gender;
+        }
+
+        public static AccountStatus CaptureStatus()
+        {
+            Console.WriteLine("1.Saving \n2.FixedDeposit\n3.FixedDeposit\n4.RecurringDeposit\n5.NRI");
+
+            AccountStatus status = (AccountStatus)Enum.Parse(typeof(AccountStatus), Console.ReadLine());
+            return status;
+        }
+    }
+}
